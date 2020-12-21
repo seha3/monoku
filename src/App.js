3462f0a5550ff5cf5a1273d27e0be1dd6d68b7e0
seha13 <out_sherreraa@independencia.com.mx>
2020-12-21 15:29:08 -0600

    primera maqueta con elementos e input funcionando

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+// import TodoForm from './components/TodoForm';
+import ElevationCard from './components/ElevationCard';
+import TodoList from './components/TodoList';
+// import Input from './components/Input'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      Hola-mundo
+    <div className="todo-app">
+      <div className={classes.root}>
+        <br/>
+        <br/>
+        <ElevationCard />
+        <br/>
+        <Typography>
+          Añade las tareas que deseas realizar
+        </Typography>
+        <br/>
+        <TodoList />
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/ElevationCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/assets/desgrr.jpg"
+          title=""
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h3">
+            Lunes 21 Diciembre 2020
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            10:40
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            ¿Qué planeas hacer el día de hoy?
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
